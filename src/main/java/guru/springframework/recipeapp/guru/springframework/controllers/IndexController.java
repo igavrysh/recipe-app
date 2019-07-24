87ec85cff8a48d6529243a785fd25913549677d4
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-07-24 07:51:58 -0700

    Testing Spring Boot DevTools

--- src/main/java/guru/springframework/recipeapp/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/guru/springframework/controllers/IndexController.java
@@ -8,6 +8,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage() {
+        System.out.println("Some message to say...");
+
         return "index";
     }
 }
