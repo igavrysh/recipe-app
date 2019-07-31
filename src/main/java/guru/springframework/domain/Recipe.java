5dad633589d80e95212f99d367605795358dd5f5
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-07-31 04:46:12 -0700

    Add Lombok to all entities. Fix circular reference

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,9 +1,12 @@
 package guru.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class Recipe {
 
@@ -43,8 +46,8 @@ public class Recipe {
     public Recipe() {
     }
 
-    public void setNotes(Notes note) {
-        this.notes = note;
+    public void setNotes(Notes notes) {
+        this.notes = notes;
         notes.setRecipe(this);
     }
 
@@ -52,105 +55,5 @@ public class Recipe {
         ingredient.setRecipe(this);
         this.ingredients.add(ingredient);
         return this;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public Integer getPrepTime() {
-        return this.prepTime;
-    }
-
-    public Integer getCookTime() {
-        return this.cookTime;
-    }
-
-    public Integer getServings() {
-        return this.servings;
-    }
-
-    public String getSource() {
-        return this.source;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    public String getDirections() {
-        return this.directions;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        return this.ingredients;
-    }
-
-    public Byte[] getImage() {
-        return this.image;
-    }
-
-    public Difficulty getDifficulty() {
-        return this.difficulty;
-    }
-
-    public Notes getNotes() {
-        return this.notes;
-    }
-
-    public Set<Category> getCategories() {
-        return this.categories;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
     }
 }
