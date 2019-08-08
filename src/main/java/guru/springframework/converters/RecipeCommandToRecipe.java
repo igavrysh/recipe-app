a36d85ce1a77099a8f318c24474e5781feae1f8a
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-08-08 10:01:43 -0700

    Add Command and Converter -s classes

---
+++ src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
@@ -0,0 +1,31 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.domain.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
+
+    private final CategoryCommandToCategory categoryConverter;
+    private final IngredientCommandToIngredient ingredientConverter;
+    private final NotesCommandToNotes notesConverter;
+
+    public RecipeCommandToRecipe(CategoryCommandToCategory categoryConverter,
+                                 IngredientCommandToIngredient ingredientConverter,
+                                 NotesCommandToNotes notesConverter) {
+        this.categoryConverter = categoryConverter;
+        this.ingredientConverter = ingredientConverter;
+        this.notesConverter = notesConverter;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand recipeCommand) {
+        return null;
+    }
+}
