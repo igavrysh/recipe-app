4540fab1e01eb56e030a901aaf78466841a5070a
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-08-08 20:22:31 -0700

    Add remaining converters tests

--- src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
+++ src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
@@ -25,7 +25,33 @@ public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
     @Synchronized
     @Nullable
     @Override
-    public Recipe convert(RecipeCommand recipeCommand) {
-        return null;
+    public Recipe convert(RecipeCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(source.getId());
+        recipe.setCookTime(source.getCookTime());
+        recipe.setPrepTime(source.getPrepTime());
+        recipe.setDescription(source.getDescription());
+        recipe.setDifficulty(source.getDifficulty());
+        recipe.setDirections(source.getDirections());
+        recipe.setServings(source.getServings());
+        recipe.setSource(source.getSource());
+        recipe.setUrl(source.getUrl());
+        recipe.setNotes(notesConverter.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0) {
+            source.getCategories().forEach(
+                    category -> recipe.getCategories().add(categoryConverter.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0) {
+            source.getIngredients().forEach(
+                    ingredient -> recipe.getIngredients().add(ingredientConverter.convert(ingredient)));
+        }
+
+        return recipe;
     }
 }
