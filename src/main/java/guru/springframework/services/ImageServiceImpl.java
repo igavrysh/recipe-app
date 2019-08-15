0f0fa26ef1a72f08f7bc0af42b596ed260d3041f
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-08-15 10:50:46 -0700

    Add test for persisting image in database

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +8,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
     public void saveImageFile(Long id, MultipartFile file) {
